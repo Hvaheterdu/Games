06a21075d8e1fc84e8a5ed8368c993be58d88837
Burhan Sarfraz <burhans941@gmail.com>
2020-11-29 22:49:53 +0100

    Syntax change

--- RockPaperScissor/src/game.py
+++ RockPaperScissor/src/game.py
@@ -16,8 +16,7 @@ THIS_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
 # Images used
 ROCK_IMG = pygame.image.load(os.path.join(THIS_FOLDER, 'image/rock.jpg'))
-PAPER_IMG = pygame.image.load(os.path.join(
-    THIS_FOLDER, 'image/paper.png'))
+PAPER_IMG = pygame.image.load(os.path.join(THIS_FOLDER, 'image/paper.png'))
 SCISSOR_IMG = pygame.image.load(
     os.path.join(THIS_FOLDER, 'image/scissor.jpg'))
 IMG = [ROCK_IMG, PAPER_IMG, SCISSOR_IMG]
