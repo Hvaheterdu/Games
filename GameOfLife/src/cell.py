f60e0aa313eb3f143f71d1082a1c6e4789e3a9f2
Burhan Sarfraz <36539269+Hvaheterdu@users.noreply.github.com>
2021-04-28 23:42:05 +0200

    Update cell.py

--- GameOfLife/src/cell.py
+++ GameOfLife/src/cell.py
@@ -5,25 +5,25 @@ class Cell:
     """ Class represents a cell in the overall game board """
 
     def __init__(self) -> None:
-        """All cells are dead to begin with """
+        """All cells are dead to begin with"""
         self._status = "Dead"
 
     def set_dead(self) -> None:
-        """Set cell status to dead """
+        """Set cell status to dead"""
         self._status = "Dead"
 
     def set_alive(self) -> None:
-        """Set cell status to alive """
+        """Set cell status to alive"""
         self._status = "Alive"
 
     def is_alive(self) -> bool:
-        """Check if cell is alive or dead """
+        """Check if cell is alive or dead"""
         if self._status == "Alive":
             return True
         return False
 
     def get_status(self) -> str:
-        """Return a character for dead and alive """
+        """Return a character for dead and alive"""
         if self._status is "Alive":
             return 'O'
         return "."
